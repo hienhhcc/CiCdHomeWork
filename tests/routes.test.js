d5ca7534115c408c9f9d8464a7b65d19ec1011ee
hienhhcc <thachdau16t@gmail.com>
2021-05-27 17:50:57 +0700

    delete unnessary

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,17 +3,10 @@ const request = require('supertest');
 
 let server;
 beforeAll(() => {
-  // jest.useFakeTimers();
-  // mongoose.connect(
-  //   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.y0eny.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`,
-  //   { useNewUrlParser: true, useUnifiedTopology: true }
-  // );
-  // mongoose.Promise = Promise;
   server = app.listen(process.env.PORT);
 });
 
 afterAll((done) => {
-  // mongoose.connection.close();
   server.close(done);
 });
 
