e7c7dc22686e1478ceaa1f1e142e54ec4594fe97
hienhhcc <thachdau16t@gmail.com>
2021-05-27 13:32:44 +0700

    add change

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get('/hahaha', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
+app.get('/hihihi', function (req, res) {
+  res.status(200).json({ name: 'hienhhcc' });
+});
+
 require('./middlewares/routes.mdw')(app);
 
 // 404 error for unknown api request
@@ -46,15 +50,3 @@ app.use(errorHandler);
 
 module.exports = app;
 
-// mongoose.connect(
-//   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.y0eny.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`,
-//   { useNewUrlParser: true, useUnifiedTopology: true }
-// );
-// mongoose.Promise = Promise;
-
-// const server = app.listen(PORT, () => {
-//   console.log(`Server is running on PORT ${PORT}`);
-// });
-
-// const io = require('./utils/socketio').init(server);
-// socketService.listenToConnectionEvent(io);
