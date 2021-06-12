67f6ff67422d028844964d252f53b49d16a0a07e
hienhhcc <thachdau16t@gmail.com>
2021-06-12 13:56:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,130 +1,143 @@
 //3rd party library
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const morgan = require("morgan");
-const httpStatus = require("http-status");
-const passport = require("passport");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const httpStatus = require('http-status');
+const passport = require('passport');
 
-const { errorConverter, errorHandler } = require("./middlewares/error.mdw");
-const ApiError = require("./utils/ApiError");
+const { errorConverter, errorHandler } = require('./middlewares/error.mdw');
+const ApiError = require('./utils/ApiError');
 
-require("dotenv").config();
+require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.status(200).json("Hello world");
+app.get('/', (req, res) => {
+  res.status(200).json('Hello world');
 });
 
-app.get("/hahaha", function (req, res) {
-  res.status(200).json({ name: "john" });
+app.get('/hahaha', function (req, res) {
+  res.status(200).json({ name: 'john' });
 });
 
-app.get("/hihihi", function (req, res) {
-  res.status(200).json({ name: "hienhhcc" });
+app.get('/hihihi', function (req, res) {
+  res.status(200).json({ name: 'hienhhcc' });
 });
 
-app.post("/user", function (req, res) {
-  res.status(200).json({ name: "minhhoang" });
+app.get('/lambada', function (req, res) {
+  res.status(200).json({ name: 'hienhhcc' });
 });
 
-app.get("/user/:id", function (req, res) {
-  if (req.params.id === "101") {
-    return res.json("User 101 found");
+app.post('/user', function (req, res) {
+  res.status(200).json({ name: 'minhhoang' });
+});
+
+app.get('/user/:id', function (req, res) {
+  if (req.params.id === '101') {
+    return res.json('User 101 found');
   }
-  return res.status(404).json("User not found !!!");
+  return res.status(404).json('User not found !!!');
 });
 
-app.get("/hello", function (req, res) {
-  res.send("hello");
+app.get('/hello', function (req, res) {
+  res.send('hello');
 });
 
-app.get("/hi", function (req, res) {
-  console.log("haha");
-  res.cookie("ettique", "hello"); // set ettique = hello
+app.get('/hi', function (req, res) {
+  console.log('haha');
+  res.cookie('ettique', 'hello'); // set ettique = hello
   res.send();
 });
 
-app.get("/post/:id", function (req, res) {
+app.get('/post/:id', function (req, res) {
   if (req.params.id == 1) {
-    return res.json({ title: "This is a post" });
+    return res.json({ title: 'This is a post' });
   } else {
-    return res.status(406).json({ message: "PostId invalid" });
+    return res.status(406).json({ message: 'PostId invalid' });
   }
 });
 
-app.get("/post/:id/comments", function (req, res) {
+app.get('/post/:id/comments', function (req, res) {
   if (req.params.id == 1) {
-    return res.json({ commentId: 1, content: "This is a comment" });
+    return res.json({ commentId: 1, content: 'This is a comment' });
   } else {
-    return res.status(406).json({ message: "PostId invalid" });
+    return res.status(406).json({ message: 'PostId invalid' });
   }
 });
 
-app.post("/post", function (req, res) {
+app.post('/post', function (req, res) {
   if (req.body.title == null) {
-    return res.status(400).json({ message: "Required title" });
+    return res.status(400).json({ message: 'Required title' });
   }
   if (req.body.content == null) {
-    return res.status(400).json({ message: "Required content" });
+    return res.status(400).json({ message: 'Required content' });
   }
   return res.json({ title: req.body.title, content: req.body.content });
 });
 
-
-app.get("/api/users/:userid", function (req, res) {
+app.get('/api/users/:userid', function (req, res) {
   if (req.params.userid == null) {
-    return res.status(400).json({ message: "Invalid" });
+    return res.status(400).json({ message: 'Invalid' });
   }
-  
+
   return res.json([
-    {name:'ho hieu',email:"hieu1@gmail.com"},
-    {name:'ho hieu 2',email:"hieu2@gmail.com"},
+    { name: 'ho hieu', email: 'hieu1@gmail.com' },
+    { name: 'ho hieu 2', email: 'hieu2@gmail.com' },
   ]);
 });
 
-app.get("/api/users", function (req, res) {
-  return res.json(
-    {name:'ho hieu',email:"hieu1@gmail.com"},
-  );
+app.get('/api/users', function (req, res) {
+  return res.json({ name: 'ho hieu', email: 'hieu1@gmail.com' });
 });
 
-    app.post("/api/users/register", function (req, res) {
-      if (!req.body.email) {
-        return res.status(400).json({ message: "The request is missing a required parameter `email`" });
-      }
-      if (!req.body.first_name) {
-        return res.status(400).json({ message: "The request is missing a required parameter `first_name`" });
-      }
-      if (!req.body.last_name) {
-        return res.status(400).json({ message: "The request is missing a required parameter `last_name`" });
-      }
-      if (!req.body.password ) {
-        return res.status(400).json({ message: "The request is missing a required parameter `password`" });
-      }
-      res.status(201).json({success:true});
-    });
-
-
-app.put("/api/users/:userid/roles", function (req, res) {
-
-  return res.status(200).json(
-    {name:'ho hieu',email:"hieu1@gmail.com",role:"user"}
-    );
+app.post('/api/users/register', function (req, res) {
+  if (!req.body.email) {
+    return res
+      .status(400)
+      .json({ message: 'The request is missing a required parameter `email`' });
+  }
+  if (!req.body.first_name) {
+    return res
+      .status(400)
+      .json({
+        message: 'The request is missing a required parameter `first_name`',
+      });
+  }
+  if (!req.body.last_name) {
+    return res
+      .status(400)
+      .json({
+        message: 'The request is missing a required parameter `last_name`',
+      });
+  }
+  if (!req.body.password) {
+    return res
+      .status(400)
+      .json({
+        message: 'The request is missing a required parameter `password`',
+      });
+  }
+  res.status(201).json({ success: true });
 });
 
-require("./middlewares/routes.mdw")(app);
+app.put('/api/users/:userid/roles', function (req, res) {
+  return res
+    .status(200)
+    .json({ name: 'ho hieu', email: 'hieu1@gmail.com', role: 'user' });
+});
+
+require('./middlewares/routes.mdw')(app);
 
 // 404 error for unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, "Route not found"));
+  next(new ApiError(httpStatus.NOT_FOUND, 'Route not found'));
 });
 
 // convert error to ApiError, if needed
